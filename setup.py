9456601f9092e4000f51399436509a564da68bd7
anjianshi <anjianshi@gmail.com>
2015-04-23 18:18:28 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='peewee-simple-migrate',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/anjianshi/peewee-simple-migrate',
     license='MIT',
     author='anjianshi',
